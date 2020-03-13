8eee29a1f559ac304a1bcab2e6f0a16d6d575533
Edwin Marroquin <edwinmarroquin@icasa.com.gt>
2020-03-13 12:02:37 -0600

    Generacion de Utilidades

---
+++ Util/Printer.cs
@@ -0,0 +1,24 @@
+using System;
+using static System.Console;
+namespace libPiky.Util
+{
+    public class Printer
+    {
+        public static void DrawLine (int size = 0)
+        {
+            WriteLine("".PadLeft(size,'='));
+        }
+        public static void WriteTiTle(string titulo)
+        {
+            var longitud = titulo.Length*3;
+            DrawLine(longitud);
+            WriteLine($"|{titulo}|");
+            DrawLine(longitud);
+        }
+        public static void PrecionarEnter()
+        {
+            WriteLine("Precione Enter Para Continuar");
+            ReadLine();
+        }
+    }
+}
